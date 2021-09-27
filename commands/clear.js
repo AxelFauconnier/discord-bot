ce9a47eb142a254fafade2cad62c4521f25975ef
Axel Fauconnier <akafuma.af@gmail.com>
2021-09-27 11:53:52 +0200

    implemented clear command

--- commands/clear.js
+++ commands/clear.js
@@ -1,11 +1,21 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { getVoiceConnection } = require('@discordjs/voice')
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('clear')
         .setDescription('clear the queue and skip current song'),
     async execute(interaction) {
-        await interaction.reply({ content: 'to implement', ephemeral: true });
-        // Reset queue, pos player.stop()
+        const connection = getVoiceConnection(interaction.guildId);
+
+        if (!connection) {
+            await interaction('MonkaS');
+            return;
+        }
+
+        connection.queue = [];
+        connection.nextSongPos = 0;
+        connection.player.stop();
+        await interaction.reply({ content: 'queue cleared', ephemeral: false });
     }
 };
